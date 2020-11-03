aa5df4775059e0195ef0e35aa03f305079f65cc2
tdquang <tdquang138k@gmail.com>
2020-11-03 10:47:03 +0700

    Added: Chapter 4 HOFs

---
+++ python/chapter_4.py
@@ -0,0 +1,8 @@
+#
+# C H A P T E R  1
+# ------------------------
+# H I G H  O R D E R  F U N C T I O N
+# a HOG function simply is a function that takes one or more functions as arguments and/or produces a function as a result
+
+if __name__ == '__main__':
+    pass
